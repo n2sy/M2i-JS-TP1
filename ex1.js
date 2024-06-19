888718e0ffc3a67ec12dc3b24f3bacc8653ede9f
Nidhal Jelassi <nidhal.jelassi@fsegt.utm.tn>
2024-06-19 12:01:02 +0100

    first commit

---
+++ ex1.js
@@ -0,0 +1,7 @@
+var txt = "Hello World";
+txt = txt.replace("World", "Universe");
+console.log(txt);
+
+var str = "Android,Javascript,Web";
+let tab = str.split(",");
+console.log(tab);
---
+++ ex2.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("btn-valider");
+const inputPrenom = document.getElementById("inp-prenom");
+const inputNom = document.getElementById("inp-nom");
+const inputAge = document.getElementById("inp-age");
+btn.addEventListener("click", camille);
+
+function camille() {
+  let msg = `Je m'appelle ${inputPrenom.value} ${inputNom.value} et j'ai ${inputAge.value} ans`;
+  alert(msg);
+}
